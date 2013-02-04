630a9f6ccdc4be84a7cc60bca1bf7ad192cfbbe1
Pelly <marcelo.eduardo.gonzalez@gmail.com>
2013-02-04 13:30:47 -0300

    select

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130204124122) do
+ActiveRecord::Schema.define(:version => 20130204144820) do
 
   create_table "ciudades", :force => true do |t|
     t.integer  "region_id"
